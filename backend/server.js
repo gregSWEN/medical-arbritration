bb1d053eefeea3bc89d6b38ce0a791232b4a4d1a
Greg Villafane <gjv4512@g.rit.edu>
2025-09-21 15:59:13 -0500

    deploy script

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,4 @@ const { createApp } = require("./app");
 const app = createApp();
 const port = process.env.PORT || 5174;
 app.listen(port, () => console.log(`API listening on :${port}`));
+// change to run the deploy
